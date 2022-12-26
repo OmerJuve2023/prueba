8fa887ee6795c49edc5f3c7a4cbe27bc86d5fe56
OmerGit <rafaelxdale@gmail.com>
2022-12-25 19:59:56 -0500

    Initial commit

---
+++ src/main/java/proselecsac/proselec/controller/controlador.java
@@ -0,0 +1,15 @@
+package proselecsac.proselec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controlador {
+    @GetMapping("/name")
+    public String hello() {
+        return """
+                hola que tal como estan,
+                pero te dejo y me marcho para siempre
+                """;
+    }
+}
