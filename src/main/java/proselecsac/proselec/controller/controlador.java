b2f62ba91e11bc2ee5bcc6cf13fc4ae643620eaf
OmerGit <rafaelxdale@gmail.com>
2022-12-25 23:36:11 -0500

    enabler

--- src/main/java/proselecsac/proselec/controller/controlador.java
+++ src/main/java/proselecsac/proselec/controller/controlador.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controlador {
-    @GetMapping("/name")
+    @GetMapping("/index")
     public String hello() {
         return """
                 hola que tal como estan,
